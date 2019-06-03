b934e786edddd4065883e1797659448cfff8d130
Isaias Ismael Zelada Campos <50118730+Arkon117@users.noreply.github.com>
2019-06-02 22:44:52 -0600

    Add files via upload

---
+++ layouts/special_menu.php
@@ -0,0 +1,39 @@
+<ul>
+    <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Productos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="producto.php">Administrar productos</a> </li>
+       <li><a href="add_producto.php">Agregar producto</a> </li>
+   </ul>
+  </li>
+    <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Ventas</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="ventas.php">Administrar ventas</a> </li>
+         <li><a href="add_venta.php">Agregar venta</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Imgenes</span>
+    </a>
+  </li>
+    <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de ventas</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="reporte_venta.php">Ventas por fecha </a></li>
+        <li><a href="venta_mensual.php">Ventas mensuales</a></li>
+        <li><a href="ventas_diarias.php">Ventas diarias</a> </li>
+      </ul>
+  </li>
+</ul>
---
+++ layouts/user_menu.php
@@ -0,0 +1,29 @@
+<ul>
+  <li>
+    <a href="inicio.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Ventas</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="ventas.php">Administrar ventas</a> </li>
+         <li><a href="add_venta.php">Agregar venta</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de ventas</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="reporte_venta.php">Ventas por fecha </a></li>
+        <li><a href="venta_mensual.php">Ventas mensuales</a></li>
+        <li><a href="ventas_diarias.php">Ventas diarias</a> </li>
+      </ul>
+  </li>
+</ul>
